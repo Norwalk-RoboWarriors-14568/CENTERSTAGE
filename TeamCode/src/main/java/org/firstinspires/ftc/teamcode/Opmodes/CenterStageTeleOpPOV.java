f87b4c0515922e4dd8384aa78616f6825432597d
NRobowarriors <56520176+NRobowarriors@users.noreply.github.com>
2023-09-14 19:36:12 -0500

    Revert "Update CenterStageTeleOpPOV.java"
    This reverts commit 3778f6397494f45d76e9d3638387d8c7335f7270.

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Opmodes/CenterStageTeleOpPOV.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Opmodes/CenterStageTeleOpPOV.java
@@ -1,8 +1,5 @@
 package org.firstinspires.ftc.teamcode.Opmodes;
 
-import static com.qualcomm.robotcore.hardware.DcMotor.ZeroPowerBehavior.BRAKE;
-import static com.qualcomm.robotcore.hardware.DcMotor.ZeroPowerBehavior.FLOAT;
-
 import com.qualcomm.robotcore.eventloop.opmode.OpMode;
 import com.qualcomm.robotcore.hardware.DcMotorSimple;
 import com.qualcomm.robotcore.hardware.Servo;
@@ -13,9 +10,7 @@ import java.lang.Math;
 @TeleOp(name = "POV Drive")
 
 public class CenterStageTeleOpPOV extends OpMode {
-    private DcMotor fl, bl, fr, br, armAngle, armHeight;
-    private Servo pixelFlipper, pin;
-    private CRServo intakeLeft, intakeRight;
+    private DcMotor fl, bl, fr, br;
     private boolean mecanumDriveMode = true;
     private float mecanumStrafe = 0;
 
@@ -26,31 +21,13 @@ public class CenterStageTeleOpPOV extends OpMode {
         fr = hardwareMap.dcMotor.get("rightFront");
         br = hardwareMap.dcMotor.get("rightRear");
 
-
-        armAngle = hardwareMap.dcMotor.get("armAngle");
-        armHeight = hardwareMap.dcMotor.get("armHeight");
-
-        pixelFlipper = hardwareMap.servo.get("pixelFlipper");
-        pin = hardwareMap.servo.get("pin");
-
-        intakeLeft = hardwareMap.crservo.get("intakeLeft");
-        intakeRight = hardwareMap.crservo.get("intakeRight");
-
-
         fr.setDirection(DcMotor.Direction.REVERSE);
         br.setDirection(DcMotor.Direction.REVERSE);
-        intakeRight.setDirection(DcMotorSimple.Direction.REVERSE);
 
-        setBehavior(fl, FLOAT);
-        setBehavior(bl, FLOAT);
-        setBehavior(fr, FLOAT);
-        setBehavior(br, FLOAT);
-
-        setBehavior(armAngle, BRAKE);
-        setBehavior(armHeight, BRAKE);
-
-
-
+        fl.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.FLOAT);
+        bl.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.FLOAT);
+        fr.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.FLOAT);
+        br.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.FLOAT);
         telemetry.addLine("Init Opmode");
     }
 
@@ -60,41 +37,24 @@ public class CenterStageTeleOpPOV extends OpMode {
 
 
         if ((gamepad1.left_stick_x) > 0.15){
-            drive(-gamepad1.left_stick_x / 2, gamepad1.left_stick_x / 2, gamepad1.left_stick_x / 2, -gamepad1.left_stick_x / 2);
+            fl.setPower((-gamepad1.left_stick_x) / 2);
+            bl.setPower((gamepad1.left_stick_x) / 2);
+            fr.setPower((gamepad1.left_stick_x) / 2);
+            br.setPower((-gamepad1.left_stick_x) / 2);
         } else {
-            drive((gamepad1.left_stick_y + -mecanumStrafe) / 2, (gamepad1.left_stick_y + mecanumStrafe) / 2, (gamepad1.left_stick_y + mecanumStrafe) / 2, (gamepad1.left_stick_y + -mecanumStrafe) / 2);
+            fl.setPower((gamepad1.left_stick_y + -mecanumStrafe) / 2);
+            bl.setPower((gamepad1.left_stick_y + mecanumStrafe) / 2);
+            fr.setPower((gamepad1.left_stick_y + mecanumStrafe) / 2);
+            br.setPower((gamepad1.left_stick_y + -mecanumStrafe) / 2);
         }
 
         if ((gamepad1.right_stick_x) > 0.2){
-            drive(-gamepad1.right_stick_x *2,-gamepad1.right_stick_x  *2, gamepad1.right_stick_x * 2, gamepad1.right_stick_x  *2);
+            fl.setPower(-gamepad1.right_stick_x *2);
+            bl.setPower(-gamepad1.right_stick_x  *2);
+            fr.setPower(gamepad1.right_stick_x * 2);
+            br.setPower(gamepad1.right_stick_x  *2);
         }
 
-        if (gamepad2.right_stick_y > 0.1){
-            armHeight.setPower(0.4);
-        } else if (gamepad2.right_stick_y < -0.1) {
-            armHeight.setPower(-0.4);
-        } else {
-            armHeight.setPower(0);
-        }
-
-        if (gamepad2.left_stick_y > 0.1) {
-            armAngle.setPower(0.4);
-        } else if (gamepad2.left_stick_y < -1) {
-            armAngle.setPower(-0.4);
-        } else {
-            armAngle.setPower(0);
-        }
-
-        if (gamepad2.right_bumper) {
-            intakeRight.setPower(1);
-            intakeLeft.setPower(1);
-        } else if (gamepad2.left_bumper) {
-            intakeRight.setPower(-1);
-            intakeLeft.setPower(-1);
-        } else {
-            intakeRight.setPower(0);
-            intakeLeft.setPower(0);
-        }
     }
     @Override
     public void stop() {
@@ -102,15 +62,12 @@ public class CenterStageTeleOpPOV extends OpMode {
         telemetry.addLine("Stopped");
     }
 
-    public void drive(double frontLeft, double backLeft, double frontRight, double backRight) {
-        fl.setPower(frontLeft);
-        bl.setPower(backLeft);
-        fr.setPower(frontRight);
-        br.setPower(backRight);
+    public void drive(double left, double right) {
+        fl.setPower(left);
+        bl.setPower(left);
+        fr.setPower(right);
+        br.setPower(right);
 
-    }
-    public void setBehavior(DcMotor motor, DcMotor.ZeroPowerBehavior Behavior){
-        motor.setZeroPowerBehavior(Behavior);
     }
 
 }
