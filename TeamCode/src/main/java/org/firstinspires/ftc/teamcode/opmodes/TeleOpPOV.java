f8dcaf128f2e161531df3a6f6c2b3c0ab3e766a5
NRobowarriors <56520176+NRobowarriors@users.noreply.github.com>
2023-10-29 18:06:28 -0500

    Update TeleOpPOV.java

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmodes/TeleOpPOV.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmodes/TeleOpPOV.java
@@ -3,6 +3,8 @@ package org.firstinspires.ftc.teamcode.opmodes;
 import static com.qualcomm.robotcore.hardware.DcMotor.ZeroPowerBehavior.BRAKE;
 import static com.qualcomm.robotcore.hardware.DcMotor.ZeroPowerBehavior.FLOAT;
 
+import static java.lang.Math.abs;
+
 import com.qualcomm.robotcore.eventloop.opmode.OpMode;
 import com.qualcomm.robotcore.hardware.DcMotorSimple;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
@@ -11,6 +13,7 @@ import com.qualcomm.robotcore.hardware.DcMotor;
 
 @TeleOp(name = "POV Drive")
 
+<<<<<<< Updated upstream
 public class TeleOpPOV extends OpMode {
     private DcMotor fl, bl, fr, br, armAngle, armHeight, lift, intakeLeft;
 
@@ -19,6 +22,15 @@ public class TeleOpPOV extends OpMode {
     private boolean drivePOV = true;
     private float mecanumStrafe = 0, dominantXJoystick = 0;
 
+=======
+public class        TeleOpPOV extends OpMode {
+    private DcMotor fl, bl, fr, br, armAngle, armHeight, lift, intakeLeft;
+
+    private CRServo  intakeRight;
+    private boolean mecanumDriveMode = true;
+    private float mecanumStrafe = 0, dominantXJoystick = 0;
+    boolean drivePOV = true;
+>>>>>>> Stashed changes
     @Override
     public void init() {
         fl = hardwareMap.dcMotor.get("leftFront");
@@ -26,19 +38,25 @@ public class TeleOpPOV extends OpMode {
         fr = hardwareMap.dcMotor.get("rightFront");
         br = hardwareMap.dcMotor.get("rightRear");
 
-
         armAngle = hardwareMap.dcMotor.get("armAngle");
         armHeight = hardwareMap.dcMotor.get("armHeight");
 
         lift = hardwareMap.dcMotor.get("lift");
 
         intakeLeft = hardwareMap.dcMotor.get("intakeLeft");
+<<<<<<< Updated upstream
         //intakeRight = hardwareMap.crservo.get("intakeRight");
+=======
+>>>>>>> Stashed changes
 
 
         fr.setDirection(DcMotor.Direction.REVERSE);
         br.setDirection(DcMotor.Direction.REVERSE);
+<<<<<<< Updated upstream
         //intakeRight.setDirection(DcMotorSimple.Direction.REVERSE);
+=======
+       // intakeRight.setDirection(DcMotorSimple.Direction.REVERSE);
+>>>>>>> Stashed changes
 
         setBehavior(fl, FLOAT);
         setBehavior(bl, FLOAT);
@@ -48,7 +66,10 @@ public class TeleOpPOV extends OpMode {
         setBehavior(armAngle, BRAKE);
         setBehavior(armHeight, BRAKE);
         setBehavior(lift, BRAKE);
+<<<<<<< Updated upstream
 
+=======
+>>>>>>> Stashed changes
 
         telemetry.addLine("Init Opmode");
     }
@@ -57,36 +78,109 @@ public class TeleOpPOV extends OpMode {
     public void loop(){
         mecanumStrafe = gamepad1.left_stick_x;
 
+        if (gamepad1.a && drivePOV){
+            drivePOV = false;
+        } else if (gamepad1.a && !drivePOV){
+            drivePOV = true;
+        }
+        mecanumStrafe = gamepad1.left_stick_x;
 
-        if ((gamepad1.left_stick_x) > 0.15){
-            drive(-gamepad1.left_stick_x / 2, gamepad1.left_stick_x / 2, gamepad1.left_stick_x / 2, -gamepad1.left_stick_x / 2);
+        if (drivePOV) {
+            if ((gamepad1.left_stick_x) > 0.15){
+                drive(-gamepad1.left_stick_x / 2, gamepad1.left_stick_x / 2, gamepad1.left_stick_x / 2, -gamepad1.left_stick_x / 2);
+            } else {
+                drive((gamepad1.left_stick_y + -mecanumStrafe) / 2, (gamepad1.left_stick_y + mecanumStrafe) / 2, (gamepad1.left_stick_y + mecanumStrafe) / 2, (gamepad1.left_stick_y + -mecanumStrafe) / 2);
+            }
+
+            if ((gamepad1.right_stick_x) > 0.2){
+                drive(gamepad1.right_stick_x *2,gamepad1.right_stick_x  *2, -gamepad1.right_stick_x * 2, -gamepad1.right_stick_x  *2);
+            } else if (gamepad1.right_stick_x < -0.2){
+                drive(-gamepad1.right_stick_x *2,-gamepad1.right_stick_x  *2, gamepad1.right_stick_x * 2, gamepad1.right_stick_x  *2);
+
+            }
         } else {
-            drive((gamepad1.left_stick_y + -mecanumStrafe) / 2, (gamepad1.left_stick_y + mecanumStrafe) / 2, (gamepad1.left_stick_y + mecanumStrafe) / 2, (gamepad1.left_stick_y + -mecanumStrafe) / 2);
+            if (abs(gamepad1.left_stick_x) > 0.15 || abs(gamepad1.right_stick_x) > 0.15) {
+                //removes negatives from joystick values, to set variable to +/- for determing stick farther from zero
+                dominantXJoystick = (abs(gamepad1.left_stick_x) - abs(gamepad1.right_stick_x));
+                mecanumDriveMode = true;
+            } else {
+                mecanumDriveMode = false;
+            }
+
+            if (mecanumDriveMode) {     //when enabled, motors will only hit 100% when strafing and driving
+
+                if (dominantXJoystick > 0) {
+                    mecanumStrafe = gamepad1.left_stick_x;
+                } else if (dominantXJoystick < 0) {
+                    mecanumStrafe = gamepad1.right_stick_x;
+                }
+
+                if (gamepad1.left_bumper) {
+                    fl.setPower((gamepad1.left_stick_y + -mecanumStrafe) / 3.0); // previously 2
+                    bl.setPower((gamepad1.left_stick_y + mecanumStrafe) / 3.0);
+                    fr.setPower((gamepad1.right_stick_y + mecanumStrafe) / 3.0);
+                    br.setPower((gamepad1.right_stick_y + -mecanumStrafe) / 3.0);
+                } else if (gamepad1.right_bumper) {
+                    fl.setPower((gamepad1.left_stick_y + -mecanumStrafe) / 1.5); // previously 2 * .5
+                    bl.setPower((gamepad1.left_stick_y + mecanumStrafe) / 1.5);
+                    fr.setPower((gamepad1.right_stick_y + mecanumStrafe) / 1.5);
+                    br.setPower((gamepad1.right_stick_y + -mecanumStrafe) / 1.5);
+                } else {
+                    fl.setPower((gamepad1.left_stick_y + -mecanumStrafe) / 2.25); // previously 2 * .75
+                    bl.setPower((gamepad1.left_stick_y + mecanumStrafe) / 2.25);
+                    fr.setPower((gamepad1.right_stick_y + mecanumStrafe) / 2.25);
+                    br.setPower((gamepad1.right_stick_y + -mecanumStrafe) / 2.25 );
+                }
+            } else if (!mecanumDriveMode ) {
+                if (gamepad1.left_bumper) {
+                    drive(gamepad1.left_stick_y * 0.8, gamepad1.right_stick_y * 0.8);
+                } else if (gamepad1.right_bumper) {
+                    drive(gamepad1.left_stick_y * 0.25, gamepad1.right_stick_y * 0.25);
+                } else {
+                    drive(gamepad1.left_stick_y * 0.5, gamepad1.right_stick_y * 0.5);
+                }
+            }
         }
 
+<<<<<<< Updated upstream
         if ((gamepad1.right_stick_x) > 0.2){
             drive(gamepad1.right_stick_x *2,gamepad1.right_stick_x  *2, -gamepad1.right_stick_x * 2, -gamepad1.right_stick_x  *2);
         }   if (gamepad1.right_stick_x < -0.2){
             drive(-gamepad1.right_stick_x *2,-gamepad1.right_stick_x  *2, gamepad1.right_stick_x * 2, gamepad1.right_stick_x  *2);
+=======
+        if (gamepad2.dpad_up){
+            lift.setPower(0.75);
+        } else if (gamepad2.dpad_down){
+            lift.setPower(-0.75);
+        } else {
+            lift.setPower(0);
+>>>>>>> Stashed changes
         }
 
         if (gamepad2.right_stick_y > 0.1){
-            armHeight.setPower(0.4);
+            armHeight.setPower(gamepad2.right_stick_y * 0.1);//arm slow
         } else if (gamepad2.right_stick_y < -0.1) {
-            armHeight.setPower(-0.4);
+            armHeight.setPower(gamepad2.right_stick_y * 0.1);
         } else {
             armHeight.setPower(0);
         }
 
         if (gamepad2.left_stick_y > 0.1) {
+<<<<<<< Updated upstream
             armAngle.setPower(gamepad2.left_stick_y * 0.2);
         } else if (gamepad2.left_stick_y < -1) {
             armAngle.setPower(gamepad2.left_stick_y * 0.2);
+=======
+            armAngle.setPower(0.4);
+        } else if (gamepad2.left_stick_y < -0.1) {
+            armAngle.setPower(-0.4);
+>>>>>>> Stashed changes
         } else {
             armAngle.setPower(0);
         }
 
         if (gamepad2.right_bumper) {
+<<<<<<< Updated upstream
             //intakeRight.setPower(1);
             intakeLeft.setPower(1);
         } else if (gamepad2.left_bumper) {
@@ -94,6 +188,15 @@ public class TeleOpPOV extends OpMode {
             intakeLeft.setPower(-1);
         } else {
             //intakeRight.setPower(0);
+=======
+           // intakeRight.setPower(1);
+            intakeLeft.setPower(1);
+        } else if (gamepad2.left_bumper) {
+         //   intakeRight.setPower(-1);
+            intakeLeft.setPower(-1);
+        } else {
+           // intakeRight.setPower(0);
+>>>>>>> Stashed changes
             intakeLeft.setPower(0);
         }
 
@@ -111,13 +214,21 @@ public class TeleOpPOV extends OpMode {
 
         }
         public void drive(double left, double right) {
+<<<<<<< Updated upstream
             fl.setPower(left);
             bl.setPower(left);
             fr.setPower(right);
             br.setPower(right);
 
         }
+=======
+        fl.setPower(left);
+        bl.setPower(left);
+        fr.setPower(right);
+        br.setPower(right);
+>>>>>>> Stashed changes
 
+    }
 
         public void setBehavior(DcMotor motor, DcMotor.ZeroPowerBehavior Behavior){
             motor.setZeroPowerBehavior(Behavior);
