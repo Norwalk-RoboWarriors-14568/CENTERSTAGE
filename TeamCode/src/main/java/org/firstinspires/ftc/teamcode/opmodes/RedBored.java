27ec19bf1fdfcf6f70eca486e3df345057798612
JamisonBice <89953910+JamisonBice@users.noreply.github.com>
2024-01-06 23:38:43 -0600

    a

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmodes/RedBored.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmodes/RedBored.java
@@ -1,6 +1,5 @@
 package org.firstinspires.ftc.teamcode.opmodes;
 
-import com.acmerobotics.roadrunner.Pose2d;
 import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.hardware.DcMotor;
