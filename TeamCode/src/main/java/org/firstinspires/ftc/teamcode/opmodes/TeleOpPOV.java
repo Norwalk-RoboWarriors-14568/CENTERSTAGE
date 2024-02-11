e523562eea6b5a6cae58fe63d350570e5ac5c976
NRobowarriors <realnorwalk@gmail.com>
2024-02-11 14:51:23 -0600

    snazzy things up for notebook

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmodes/TeleOpPOV.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmodes/TeleOpPOV.java
@@ -9,14 +9,13 @@ import com.qualcomm.robotcore.eventloop.opmode.OpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.hardware.CRServo;
 import com.qualcomm.robotcore.hardware.DcMotor;
-import com.qualcomm.robotcore.hardware.DcMotorSimple;
 import com.qualcomm.robotcore.hardware.Servo;
 
 @TeleOp(name = "POV Drive")
 
 public class TeleOpPOV extends OpMode {
-    DcMotor fl, bl, fr, br, arm1, arm2, lift, intakeLeft;
-    private Servo gun;
+    DcMotor fl, bl, fr, br, leftArmMotor, rightArmMotor, bucketMotor, intakeLeft;
+    private Servo droneLauncherServo;
     boolean jacob = false;
     private CRServo intake2;
     private Servo bucket;
@@ -31,35 +30,35 @@ public class TeleOpPOV extends OpMode {
         fr = hardwareMap.dcMotor.get("rightFront");
         br = hardwareMap.dcMotor.get("rightBack");
 
-        arm1 = hardwareMap.dcMotor.get("armLeft");
-        arm2 = hardwareMap.dcMotor.get("armRight");
+        leftArmMotor = hardwareMap.dcMotor.get("armLeft");
+        rightArmMotor = hardwareMap.dcMotor.get("armRight");
         //bucket = hardwareMap.servo.get("bucket");
 
-        lift = hardwareMap.dcMotor.get("Lift");
+        bucketMotor = hardwareMap.dcMotor.get("Lift");
         intakeLeft = hardwareMap.dcMotor.get("intakeLeft");
        intake2 = hardwareMap.crservo.get("intake2");
-        gun = hardwareMap.servo.get("gun");
+        droneLauncherServo = hardwareMap.servo.get("gun");
 
 
         fr.setDirection(DcMotor.Direction.REVERSE);
         br.setDirection(DcMotor.Direction.REVERSE);
-        arm2.setDirection(DcMotor.Direction.REVERSE);
+        rightArmMotor.setDirection(DcMotor.Direction.REVERSE);
 
         setBehavior(fl, FLOAT);
         setBehavior(bl, FLOAT);
         setBehavior(fr, FLOAT);
         setBehavior(br, FLOAT);
 
-        setBehavior(arm1, BRAKE  );
-        setBehavior(arm2, BRAKE);
-        setBehavior(lift, BRAKE);
+        setBehavior(leftArmMotor, BRAKE  );
+        setBehavior(rightArmMotor, BRAKE);
+        setBehavior(bucketMotor, BRAKE);
         telemetry.addLine("Init Opmode");
         /*
         armAngle.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
         armAngle.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
         */
-         arm1.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
-        float armTicksZero = arm1.getCurrentPosition();//sets the floor position to 0
+         leftArmMotor.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+        float armTicksZero = leftArmMotor.getCurrentPosition();//sets the floor position to 0
        // bucket.setPosition(0);
         //bucket.setBehavior();
     }
@@ -110,15 +109,15 @@ public class TeleOpPOV extends OpMode {
 
 
         if (gamepad2.right_stick_y > 0.1){
-            arm2.setPower(gamepad2.right_stick_y);//arm slow
-            arm1.setPower(gamepad2.right_stick_y);
+            rightArmMotor.setPower(gamepad2.right_stick_y);//arm slow
+            leftArmMotor.setPower(gamepad2.right_stick_y);
         } else if (gamepad2.right_stick_y < -0.1) {
-            arm2.setPower(gamepad2.right_stick_y);
-            arm1.setPower(gamepad2.right_stick_y);
+            rightArmMotor.setPower(gamepad2.right_stick_y);
+            leftArmMotor.setPower(gamepad2.right_stick_y);
 
         } else {
-            arm2.setPower(0);
-            arm1.setPower(0);
+            rightArmMotor.setPower(0);
+            leftArmMotor.setPower(0);
         }
 
         if (gamepad2.right_trigger > 0.25){
@@ -137,19 +136,19 @@ public class TeleOpPOV extends OpMode {
 
         if(gamepad2.right_bumper){
            // bucket.setPosition(0.15);//Can only use these values nothing beyond this point
-            lift.setPower(0.30);
+            bucketMotor.setPower(0.30);
         } else if (gamepad2.left_bumper){
             // bucket.setPosition(0.85);//Can only use these values nothing beyond this point
-            lift.setPower(-0.30);
+            bucketMotor.setPower(-0.30);
         }else {
-            lift.setPower(0);
+            bucketMotor.setPower(0);
         }
         //telemetry.addLine("Servo " + bucket.getPosition());
         if (gamepad2.y) {
-            gun.setPosition(1);
+            droneLauncherServo.setPosition(1);
         }
         if (gamepad2.x ){
-            gun.setPosition(0.5);
+            droneLauncherServo.setPosition(0.5);
         }
 
         telemetry.update();
@@ -180,9 +179,9 @@ public class TeleOpPOV extends OpMode {
             motor.setZeroPowerBehavior(Behavior);
         }
         public void raiseArm(double armSpeed, int newTargetAngle){
-            arm1.setTargetPosition(newTargetAngle);
-            arm1.setPower(armSpeed);
-            arm1.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+            leftArmMotor.setTargetPosition(newTargetAngle);
+            leftArmMotor.setPower(armSpeed);
+            leftArmMotor.setMode(DcMotor.RunMode.RUN_TO_POSITION);
         }
 
     }
